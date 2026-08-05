18fc2af9cff5d09a32cdafa3f88baf4e93fc2bcb
Mazzolena <mirco.mazzolena2@dxc.com>
2026-08-05 17:17:54 +0200

    spostato opi-siniis in src

--- opi-siniis/src/opi-siniis.py
+++ opi-siniis/src/opi-siniis.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).resolve().parent / "src"))
+sys.path.insert(0, str(Path(__file__).resolve().parent))
 
 from opi_siniis.__main__ import main
 
